801269c4a069e2dde9bf6105a91f5e47d62acd33
Logan O'Leary <ltoleary3@gmail.com>
2022-04-16 17:21:56 -0400

    updated waiting for connection functionality

--- shinyHunt.py
+++ shinyHunt.py
@@ -21,30 +21,33 @@ if __name__ == "__main__":
     # Get list of all Bluetooth adapters
     print("Getting bluetooth adapters")
     adapters = nx.get_available_adapters()
-    controllerIndex = []
 
     # Create Pro Controller instance
     print("Creating Pro Controller instance")
     if (len(nx.get_switch_addresses()) < 1):
         # No previous controller instance. Creating new ones
         print("Setting up for first time")
-        for i in range (0,len(adapters)):
-            index = nx.create_controller(
-                nxbt.PRO_CONTROLLER,
-                adapter_path=adapters[i])
-            controllerIndex.append(index)
+        index = nx.create_controller(
+            nxbt.PRO_CONTROLLER,
+            adapter_path=adapters[i],
+            colour_body=random_colour(),
+            colour_buttons=random_colour())
+        # Wait for connection to controller instance
+        print("Connecting...")
+        nx.wait_for_connection(index)
+        print("Connected")
     else:
         # Get previous controller instance
         print("Getting previous controller instance")
         index = nx.create_controller(
             nxbt.PRO_CONTROLLER,
-            reconnect_address=nx.get_switch_addresses())
-        controllerIndex.append(index)
-
-    # Wait for connection to last controller instance
-    print("Connecting...")
-    nx.wait_for_connection(controllerIndex[-1])
-    print("Connected")
+            reconnect_address=nx.get_switch_addresses(),
+            colour_body=random_colour(),
+            colour_buttons=random_colour())
+        # Wait for connection to last controller instance
+        print("Connecting...")
+        nx.wait_for_connection(index)
+        print("Connected")
 
     # Start game macro
     nx.macro(controllerIndex, startGame)
