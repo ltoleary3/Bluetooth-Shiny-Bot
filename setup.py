38a7d281dafde30f56c446d82b2ece6db955d515
Logan O'Leary <ltoleary3@gmail.com>
2022-04-20 20:42:02 -0400

    fixed opencv library name

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup (
     version='0.1',
     description='Emulate a switch pro-controller over bluetooth to hunt for select shiny pokemon',
     packages=find_packages(),
-    install_requires=['matplotlib', 'numpy', 'nxbt', 'cv2']
+    install_requires=['matplotlib', 'numpy', 'nxbt', 'opencv-python']
 )
