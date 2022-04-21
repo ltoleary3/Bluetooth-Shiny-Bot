9556d73862714c94b699f61fe24ad76ca3b71a30
Logan O'Leary <ltoleary3@gmail.com>
2022-04-20 20:39:10 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup (
+    name='Bluetooth-Shiny-Bot',
+    version='0.1',
+    description='Emulate a switch pro-controller over bluetooth to hunt for select shiny pokemon',
+    packages=find_packages(),
+    install_requires=['matplotlib', 'numpy', 'nxbt', 'cv2']
+)
